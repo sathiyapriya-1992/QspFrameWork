c8482ac6cd2dbcb13b3cf5304110354fa05d2848
LENOVO <LENOVO@DESKTOP-SN4Q53J>
2024-10-05 15:08:12 -1200

    QSP framework Done

---
+++ QspFramework/src/test/java/Organization/TOCreateContactWithOrganization.java
@@ -0,0 +1,10 @@
+package Organization;
+
+public class TOCreateContactWithOrganization {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Created Contact with Organization Successfully");
+	}
+
+}
